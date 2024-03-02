c9a15e0bbd1e8349adf42bfd60339d0f77c5a9f5
Luiz <lvj253@gmail.com>
2024-03-02 09:02:50 -0700

    changes

--- frontend/src/utils/addresses.ts
+++ frontend/src/utils/addresses.ts
@@ -1,9 +1,17 @@
 
 
 export const contractAddresses = {
-    51: {
+    51: { //XDC
         "BlockEstate": "0x69801C169647Ad125707Dd40096D4EDC20Bb521a",
         "PaymentToken": "0x536527976E98E253B424a3655E695D144E343341"
+    }, 
+    84532: { //BASE
+        "BlockEstate": "0xBd6ed8BAF6904a97c3125f1b6920f268fF39Aaa2",
+        "PaymentToken": "0xb0744daaf6E84855C3551CDfbCec5892A8892B86"
+    },
+    59140: {//LINEA
+        "BlockEstate": "0xBd6ed8BAF6904a97c3125f1b6920f268fF39Aaa2",
+        "PaymentToken": "0xb0744daaf6E84855C3551CDfbCec5892A8892B86"
     }
 } as {[chainId: number]: {[contractName: string]: string}};
 
